6498bd9d575cf07e7bf985a128a68b204ed6ec01
TaN-404 <tanayrataboli@gmail.com>
2025-01-22 00:20:56 +0530

    player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 const SPEED = 100
 
 func _physics_process(delta):
-	var input = Input.get_vector("ui_left", "ui_right", "ui_up" , "ui_down")
+	var input = Input.get_vector("left", "right", "up" , "down")
 
 	if input != Vector2.ZERO:
 		input = input.normalized()  # Normalize for 8-directional movement
